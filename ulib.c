000104a70e23867c0f6d270ea4f0ce6d65e47c0c
KrisUdeh <Cudeh@Hawk.iit.edu>
2019-04-16 16:38:32 -0500

    lib

--- ulib.c
+++ ulib.c
@@ -104,3 +104,31 @@ memmove(void *vdst, const void *vsrc, int n)
     *dst++ = *src++;
   return vdst;
 }
+
+int thread_create(void (*start_routine)(void*, void*), void* arg1, void* arg2)
+{
+
+}
+
+int thread_join()
+{
+ int val;
+
+ val = join(&stack);
+ return val;
+}
+
+void lock_init(lock_t* lock)
+{
+
+}
+
+void lock_acquire(lock_t* lock)
+{
+
+}
+
+void lock_release(lock_t* lock)
+{
+
+}
